a61f0e746a6ed1356831389c51b84ab9f5ffb1b2
pravin <pambre.ht@gmail.com>
2018-12-10 23:24:39 -0500

    data changes

--- io_dataload/api.py
+++ io_dataload/api.py
@@ -4,12 +4,18 @@ from flask_cors import CORS
 from flask import jsonify
 import glob
 import os
+import datetime
+import time
 from flask_mysqldb import MySQL
+import csv
+from zipfile import ZipFile
+import glob
 
 import ast
 import json
 import base64
 from bson import json_util
+import shutil 
 
 app = Flask(__name__)
 api = Api(app)
@@ -27,34 +33,45 @@ def users():
     cur = mysql.connection.cursor()
     cur.execute('''select * from csvdata''')
     rv = cur.fetchall()
-    return str(rv)
+    return str(rv)  
 
 
 class GetRecords(Resource):
-    def get(self):
-        from zipfile import ZipFile
-        zf = ZipFile('/home/pravin/project/darknet/rest_flask/HP444.zip')
-        zf.extractall('/home/pravin/project/darknet/rest_flask/')
-        zf.close()
-        import datetime, os
+    def getZip(self):
+        zip_data = glob.glob(os.getcwd()+"/data_zip/*.zip")
+        if zip_data:
+            for i in zip_data:
+                print (os.getcwd()+"/zip_backup")
+                shutil.copy(i, os.getcwd()+"/zip_backup")
+                zf = ZipFile(i)        
+                zf.extractall(os.getcwd()+"/data_zip/")
+                zf.close()
 
+    def insideFolder(self,dir_path):
+        csv_files= glob.glob(dir_path + "/*.csv")
+        for csv in csv_files:
+            self.csvFile(csv)
+    
+    def csvFile(self,csv_path):
         cur = mysql.connection.cursor()
-
-
-        mapping_file = open('54.txt','r') 
+        mapping_file = open(os.getcwd()+'/54.txt','r') 
         data = mapping_file.readlines()
         keys = []
         for da in data:
             ta = da.split("\t\t")
             keys.append(ta[2].split('\t\r\n')[0])
 
-        import csv
         count = 0
-        File = open('/home/pravin/project/darknet/rest_flask/HP444.csv')
+        File = open(csv_path)
         csv_data = csv.reader(File)
-        # csv_data = csv.reader(file('/home/pravin/project/darknet/rest_flask/HP444.csv'))
         for row in list(csv_data):
             if not '#' in row[0]:
+                date = row[0].split("/")
+                try :
+                    date = date[1]+"-"+date[2] + "-"+ date[0] + " " +row[1]
+                except:
+                    date = date
+                print(date)
                 if count:
                     cnt = 0
                     for r in row[2:]:
@@ -64,13 +81,33 @@ class GetRecords(Resource):
                         except:
                             key = None  
                         cnt +=1   
-                        cur.execute('INSERT INTO csvdata(Date, Time,matrics,value,timestamp) VALUES("{}", "{}", "{}","{}","{}")'.format(row[0],row[1],key,r,datetime.datetime.now()))
+                        cur.execute('INSERT INTO csvdata(date,unix_timestamp,metrics,value,timestamp) VALUES("{}", "{}", "{}","{}","{}")'.format(date,
+                            time.mktime(datetime.datetime.strptime(row[0], "%Y/%m/%d").timetuple()),
+                            key,r,datetime.datetime.now()))
                 count += 1
                 
-        #close the connection to the database.
         mysql.connection.commit()
-        rv = cur.fetchall()
-        return str(rv)
+        return cur.fetchall()
+
+    def get(self):
+        
+        if os.listdir(os.getcwd()+"/data_zip"):
+            self.getZip()
+        else:
+            return {'message': 'No csv found'}
+        data = ''
+        if os.listdir(os.getcwd()+"/data_zip"):
+            folder = glob.glob(os.getcwd()+"/data_zip/*")
+            
+            for i in folder:
+                if os.path.isdir(i):
+                    data = self.insideFolder(i)
+                    break #single csv file
+                elif i.endswith('.csv'):
+                    data = self.csvFile(i)
+            # for f in folder:
+            #     shutil.rmtree(f)
+        return str(data)
 
 
 api.add_resource(GetRecords, '/store')
---
+++ io_dataload/csv.sql
@@ -0,0 +1,7 @@
+ CREATE TABLE csvdata (
+ date VARCHAR(30) NOT NULL,
+ unix_timestamp VARCHAR(30) NOT NULL,
+ metrics VARCHAR(30) NOT NULL,
+ value VARCHAR(30) NOT NULL,
+ timestamp TIMESTAMP
+ )

486cb693b3b1e5485b930c54fca7bf8743742b19
pravin <pambre.ht@gmail.com>
2018-12-17 07:38:50 -0500

    sql

--- io_dataload/csv.sql
+++ io_dataload/csv.sql
@@ -1,13 +1,13 @@
  CREATE TABLE dataload (
- date VARCHAR(30) NOT NULL,
- unix_timestamp VARCHAR(30) NOT NULL,
- metrics INT NOT NULL,
- value float NOT NULL,
- date_time TIMESTAMP
+    date VARCHAR(30) NOT NULL,
+    unix_timestamp VARCHAR(30) NOT NULL,
+    metrics VARCHAR(30)  NULL,
+    value VARCHAR(30)  NULL,
+    date_time TIMESTAMP
  )
 
  CREATE TABLE maptable (
- types VARCHAR(30) NOT NULL,
- name VARCHAR(30) NOT NULL,
- metrics VARCHAR(30) NOT NULL UNIQUE,
+    types VARCHAR(30) NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    metrics VARCHAR(30) NOT NULL UNIQUE,
  )
